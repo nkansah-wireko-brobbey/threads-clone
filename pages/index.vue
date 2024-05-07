8511b57393cceaa0b8063da826c12347e20e5822
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-06 21:26:00 -0500

    Added basic skeleton for posts page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,25 @@
 <template>
-    <div>
-        <h1>Home Page</h1>
+  <MainLayout>
+    <div id="IndexPage" class="w-full overflow-auto">
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+        <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+            <div v-if="isPosts" v-for="post in posts" :key="post">
+                    {{ post }}
+            </div>
+        </div>
+      </div>
     </div>
+  </MainLayout>
 </template>
 
-<script>
-    export default {
-        
-    }
-</script>
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
 
+import {useUserStore} from '~/stores/user'
+
+const userStore = useUserStore();
+
+let posts = ref([])
+let isPosts = ref(false)
+let isLoading = ref(false)
+</script>
