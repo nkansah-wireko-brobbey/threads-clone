e33f685a47f550716a740eb8bb296b3b35322f4b
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-10 22:17:19 -0500

    Refactored index page. Removed redundant layout component

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,37 @@
 <template>
-  <MainLayout>
     <div id="IndexPage" class="w-full overflow-auto">
       <div class="mx-auto max-w-[500px] overflow-hidden">
         <div id="Posts" class="px-4 max-w-[600px] mx-auto">
             <div v-if="isPosts" v-for="post in posts" :key="post">
-                    {{ post }}
+
+                <Post :post="post" @isDeleted=""/>
+            
+
             </div>
         </div>
       </div>
     </div>
-  </MainLayout>
 </template>
 
 <script setup>
-import MainLayout from "~/layouts/MainLayout.vue";
+// import MainLayout from "~/layouts/MainLayout.vue";
 
 import {useUserStore} from '~/stores/user'
 
 const userStore = useUserStore();
 
 let posts = ref([])
-let isPosts = ref(false)
+let isPosts = ref(true)
 let isLoading = ref(false)
+ 
+onBeforeMount(()=>{
+  posts.value = [
+    {
+      name: 'Spero',
+      image: 'https://placehold.co/100',
+      text: 'This is the title',
+      picture: 'https://placehold.co/500'
+    }
+  ]
+})
 </script>
