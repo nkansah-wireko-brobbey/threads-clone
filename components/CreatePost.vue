bb4cd1f2a600fa4b71fc3ea3add4fbc07c9a5346
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-10 22:14:49 -0500

    Added create post component

---
+++ components/CreatePost.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="CreatePost" class="fixed bottom-0 z-50 h-full w-full">
+
+    </div>
+</template>
+
+<script setup>
+    import {v4 as uuidv4} from 'uuid'
+
+    import { useUserStore } from '~/stores/user';
+    const userStore = useUserStore();
+
+ 
+
+    // const client = useSupabaseClient()
+    // const user = useSupabaseUser()
+
+    let text = ref(null)
+    let isLoading = ref(false)
+
+    const modifyTextAreaHeight=()=>{
+        var textarea = document.getElementById("textarea");
+        textarea.style.height ="auto"
+        textarea.style.height = textarea.scrollHeight+"px"
+    }
+
+    let file = ref(null)
+    let fileDisplay = ref(null)
+    let fileData = ref(null)
+
+    const clearData =()=>{
+        text.value = null
+        file.value = null
+        fileDisplay.value = null
+        fileData.value = null
+    }
+
+    const onChange = () =>{
+        fileDisplay.value = URL.createObjectURL(file.value.files[0])
+        fileData.value = file.value.files[0]
+    }
+
+</script>
+
+<style lang="sass" scoped>
+    
+</style>
