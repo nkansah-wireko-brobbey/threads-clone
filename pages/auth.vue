aabc0bb1a26dfc283b5b06c07744852c13ae068b
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-11 23:20:58 -0500

    Add authentication page component with login functionality

---
+++ pages/auth.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+// const client = useSupabaseClient()
+// const user = useSupabaseUser()
+
+watchEffect(()=>{
+    if (user.value) {
+        return navigateTo('/')
+    }
+})
+
+const login = async (prov)=>{
+    const {data, error} = await client.auth.signInWithOAuth({
+        provider: prov,
+        redirectTo: window.location.origin
+    })
+
+    if(error) console.log(error)
+}
+    
+</script>
+
