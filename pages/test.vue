1353a9b97ecc91d46a0a76f3d9cd46fdcd406ad8
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-11 23:21:52 -0500

    Troubleshooting navigation

---
+++ pages/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+        <h1 class="text-white font-bold">This is a test page</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
