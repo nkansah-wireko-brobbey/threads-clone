efb263c8ad27841ba53e05d8f6b6709c260833e7
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-11 23:22:15 -0500

    Add CreatePost component for creating new threads

--- app.vue
+++ app.vue
@@ -4,6 +4,25 @@
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
+      <CreatePost
+        :class="
+        [
+          {
+            'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay
+          },
+          {
+            'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay
+          }
+        ]"
+      />
     </div>
 
 </template>
+
+<script setup>
+
+  import { useUserStore } from './stores/user';
+
+  const userStore = useUserStore();
+
+</script>
