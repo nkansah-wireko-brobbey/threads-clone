f5188d3aa9b05865e70487764fe257fbdf07bbc3
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-11 23:20:23 -0500

    Add CreatePost component template for creating new threads

--- components/CreatePost.vue
+++ components/CreatePost.vue
@@ -1,6 +1,85 @@
 <template>
     <div id="CreatePost" class="fixed bottom-0 z-50 h-full w-full">
+            <div class="bg-black h-full w-full text-white overflow-auto">
+                <div class="flex items-center justify-start py-4 max-w-[500px] mx-auto border-b border-b-gray-700">
 
+                    <button
+                    @click="userStore.isMenuOverlay = false;
+                    clearData();"
+                    class="rounded-full px-2"
+                    >
+                        <Icon name="mdi:close" size="25"/>
+                    </button>
+
+                    <div class="font-semibold text-[16px]">New Thread</div>
+
+                    
+                    
+                </div>
+                <div id="Post" class="z-40 bottom-9 max-h-[100vh-200px] w-full px-3 max-w-[500px] mx-auto">
+                    <div class="py-2 w-full">
+                        <div class="flex items-center">
+
+                            <div class="flex items-center text-white">
+                                <img src="https://picsum.photos/id/233/50" class="rounded-full h-[35px]">
+                                <div class="ml-2 font-semibold text-[18px]">
+                                    John Weeks Dev
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="relative flex items-center w-full">
+                            <div class="w-[42px] mx-auto">
+                                <div class="absolute ml-4 mt-1 top-0 w-[1px] bg-gray-700 h-full"/>
+                            </div>
+                            <div class="bg-black rounded-lg w-[calc(100%-50px)] text w-full font-light">
+                                <div class="pt-2 text-gray-300 bg-black w-full">
+                                    <textarea v-model="text" style="resize: none;" placeholder="Start a thread..." id="textarea" @input="modifyTextAreaHeight()" class="w-full bg-black outline-none"></textarea>
+                                </div>
+                                <div class="w-full">
+                                    <div class="flex flex-col gap-2 py-1">
+
+                                        <div
+                                        v-if="fileDisplay"
+                                        >
+                                        <img :src="fileDisplay" alt="" class="mx-auto w-full mt-2 mr-2 rounded-lg">
+
+                                        </div>
+
+                                        <label for="fileInput" class="cursor-pointer">
+                                            <Icon name="clarity:paperclip-line" color="#fff" size="25"/>
+                                            <input 
+                                            ref="file"
+                                            type="file"
+                                            id="fileInput"
+                                            @input="onChange"
+                                            hidden
+                                            accept=".jpg,.jpeg,.png"
+                                            />
+                                        </label>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+<button
+v-if="text"
+:disabled="isLoading"
+class="fixed bottom-0 font-bold text-lg w-full p-2 bg-black inline-block float-right p-4 border-t border-t-gray-700"
+:class="isLoading?'text-gray-600':'text-blue-600'"
+>
+<div v-if="!isLoading">Post</div>
+<div v-else class="flex items-center gap-2 justify-center">
+    <Icon name="eos-icons:bubble-loading" size="25"/>
+    Please wait...
+</div>
+
+</button>
+            </div>
     </div>
 </template>
 
