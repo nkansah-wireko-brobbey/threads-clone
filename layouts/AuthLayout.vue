0a32b8739b01f1c121ae53a5d7c9ef22ad893275
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-14 00:50:11 -0500

    feat: Add AuthLayout component for authentication pages

---
+++ layouts/AuthLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="AuthLayout" class="h-screen">
+        <div class="w-full h-screen">
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<script>
+   
+</script>
+
+<style lang="sass" scoped>
+
+</style>
